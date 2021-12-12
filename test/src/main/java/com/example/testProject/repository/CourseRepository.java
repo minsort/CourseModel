b16a69eb304832d115d6d063bff17ba071ae3878
minsort <71903774+minsort@users.noreply.github.com>
2021-12-12 21:03:33 +0300

    Delete test directory

--- test/src/main/java/com/example/testProject/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.testProject.repository;
-
-
-import com.example.testProject.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository <Course, Long>{
-}
